e62eec474079067c6f72e4ce5b1ef658c2aff6b0
kdouveno <kdouveno@users.noreply.github.com>
2018-12-27 22:25:01 +0100

    form inputs stylished

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,4 +1,9 @@
-form = {
+function KDformInit(){
+	KDform.setNumInputs();
+	KDform.setCheckInputs();
+}
+
+KDform = {
 	objForm: function(sel){
 		return $(sel).serializeArray().reduce(function(t, o){
 			t[o.name] = o.value;
@@ -9,18 +14,55 @@ form = {
 	{
 		$("input[type=number]").each((i, o) => {
 			$(o).replaceWith(`<div class="inputLike numInput">
-				<div onclick='form.numMinus(this)'>-</div>
-				<input value="`+$(o).attr("value")+`"/>
-				<div onclick='form.numPlus(this)'>+</div>
+				<div onclick='KDform.numMinus(this)'>-</div>
+				<input value="`+(isNaN($(o).attr("value")) ? 0 : $(o).attr("value"))+`" oninput="KDform.numEvent(this)" onchange="KDform.numUnEvent(this)"/>
+				<div onclick='KDform.numPlus(this)'>+</div>
 			</div>`);
 		});
 	},
+	numEvent: function(e){
+		var test = /-/.test(e.value) && !/\+/.test(e.value);
+		e.value = e.value.replace(/[^0-9]*/g, "");
+		if (test)
+			e.value = "-" + e.value;
+	},
+	numUnEvent: function(e)
+	{
+		var out = parseInt(e.value);
+		e.value = isNaN(out) ? 0 : out;
+	},
 	numPlus: (e)=>{
-		input = $(e).siblings("input").get(0);
+		var input = $(e).siblings("input").get(0);
 		input.value = parseInt(input.value) + 1;
 	},
 	numMinus: (e)=>{
-		input = $(e).siblings("input").get(0);
+		var input = $(e).siblings("input").get(0);
 		input.value = parseInt(input.value) - 1;
+	},
+	setCheckInputs: function()
+	{
+		$("input[type=checkbox]").each((i, o) => {
+			var je = $(o);
+			je.replaceWith(`<div class="inputLike checkInput `+ (je.get(0).checked ? `checked` : ``) +`">
+				<div class="under">
+					<div>on</div>
+					<div>off</div>
+				</div>
+				<div class="hover">
+					<div>on</div>
+					<div>off</div>
+				</div>
+				<input type="checkbox" oninput="KDform.checkEvent(this)" `+ (je.get(0).checked ? `checked` : ``) +`/>
+			</div>`);
+		});
+	},
+	checkEvent: (e)=>{
+		console.log(e.checked);
+		var p = $(e).parent();
+		if (e.checked)
+			p.addClass("checked");
+		else {
+			p.removeClass("checked");
+		}
 	}
 }
